d234114ba7ed8a48c729de7d360d9b44a6f5a50a
Hati <hati.goshujin@gmail.com>
2025-09-15 03:55:12 +0200

    feat: Add YOU indicator on ScreenEvaluation's rank list

--- BGAnimations/ScreenEvaluationNormal decorations/scoresUnified.lua
+++ BGAnimations/ScreenEvaluationNormal decorations/scoresUnified.lua
@@ -5,14 +5,14 @@ local t = Def.ActorFrame{}
 -- Holy fcuk yes it's finally working (inefficient as it may be)
 local function RivalScore(pn, rival)
 	return Def.ActorFrame{
-		OnCommand=function(s) s:playcommand("Set") end,
-		CurrentSongChangedMessageCommand=function(s) s:playcommand("Set") end,
-		CurrentCourseChangedMessageCommand=function(s) s:playcommand("Set") end,
-		["CurrentSteps"..ToEnumShortString(pn).."ChangedMessageCommand"]=function(s) s:queuecommand("Set") end,
-		["CurrentTrail"..ToEnumShortString(pn).."ChangedMessageCommand"]=function(s) s:queuecommand("Set") end,
-		["ProfileDisplayName"..ToEnumShortString(pn).."ChangedMessageCommand"]=function(s) s:queuecommand("Set") end,
-		SetCommand=function(s)      
-			local c = s:GetChildren()
+		OnCommand=function(self) self:playcommand('Set') end,
+		CurrentSongChangedMessageCommand=function(self) self:playcommand('Set') end,
+		CurrentCourseChangedMessageCommand=function(self) self:playcommand('Set') end,
+		['CurrentSteps'..ToEnumShortString(pn)..'ChangedMessageCommand']=function(self) self:queuecommand('Set') end,
+		['CurrentTrail'..ToEnumShortString(pn)..'ChangedMessageCommand']=function(self) self:queuecommand('Set') end,
+		['ProfileDisplayName'..ToEnumShortString(pn)..'ChangedMessageCommand']=function(self) self:queuecommand('Set') end,
+		SetCommand=function(self)      
+			local c = self:GetChildren()
 			
 			local SongOrCourse, StepsOrTrail
 			if GAMESTATE:IsCourseMode() then
@@ -48,50 +48,69 @@ local function RivalScore(pn, rival)
 			c.ScoreName:visible(true)
 			
 			local name
-			if (score:IsFillInMarker()) then
-				name = PROFILEMAN:GetProfile(pn):GetDisplayName() -- Is set during profile select
+			if score:IsFillInMarker() then
+				c.ScoreName:settext(PROFILEMAN:GetProfile(pn):GetDisplayName())
+				local nameRightX = c.ScoreName:GetX() + c.ScoreName:GetWidth()
+				c.YouIndicator:visible(true)
+				c.YouIndicator:halign(0):x(nameRightX + 8)
+				c.YouIndicator:glowblink():effectcolor1(color('1,1,1,0')):effectcolor2(color('1,1,1,0.2')):effectperiod(0.2)
 			else
-				name = score:GetName()
+				c.ScoreName:settext(score:GetName())
+				c.YouIndicator:visible(false)
+				c.YouIndicator:stopeffect()
 			end
-			c.ScoreName:settext(name)
 			
-			s:playcommand('SetScore', { Stats = score, Steps = StepsOrTrail })
+			self:playcommand('SetScore', { Stats = score, Steps = StepsOrTrail })
 		end,
 		Def.BitmapText{
-			Font="_avenirnext lt pro bold/25px",
-			Text=THEME:GetString("ScreenEvaluation","RIVAL"..rival),
-			InitCommand=function(s) s:halign(1):x(-130):maxwidth(140):strokecolor(Alpha(Color.Black,0.4)) end,
+			Font='_avenirnext lt pro bold/25px',
+			Text=THEME:GetString('ScreenEvaluation','RIVAL' .. rival),
+			InitCommand=function(self)
+				self:halign(1):x(-130):maxwidth(140):strokecolor(Alpha(Color.Black, 0.4))
+			end,
 		},
 		Def.BitmapText{
 			Name='ScoreName',
 			Font='_avenirnext lt pro bold/25px',
-			InitCommand=function(s) s:zoom(1):halign(0):x(-110):strokecolor(Color.Black) end,
+			InitCommand=function(self)
+				self:zoom(1):halign(0):x(-117):strokecolor(Color.Black)
+			end,
+		},
+		Def.BitmapText{
+			Name='YouIndicator',
+			Font='_avenirnext lt pro bold/16px',
+			Text='YOU',
+			InitCommand=function(self)
+				self:visible(false)
+				self:zoom(0.95):valign(0.75):skewx(-0.15)
+				self:halign(0.5):x(70)
+				self:diffusecolor(color('#ffd800')):strokecolor(Color.Black)
+			end,
 		},
 		ScoreAndGrade.CreateScoreActor{	
 			Name='Score',
 			Font='_avenirnext lt pro bold/25px',
 			InitCommand=function(self)
-				self:x(215):zoom(1):halign(1):strokecolor(Color.Black)
+				self:x(220):zoom(1):halign(1):strokecolor(Color.Black)
 			end,
 		},
 		ScoreAndGrade.CreateGradeActor{
 			Name='GradeFrame',
 			InitCommand=function(self)
-				self:x(245):zoom(1.1)
+				self:x(247):zoom(1.1)
 			end,
 		}
-
 	}
 end
 
 t[#t+1] = Def.Sprite{
-	Texture="ScoreFrame.png",
+	Texture='ScoreFrame.png',
 }
 
 for i=1,6 do
-	t[#t+1] = RivalScore(pn,i)..{
-		InitCommand=function(s)
-			s:y((i*43)-150)
+	t[#t+1] = RivalScore(pn, i) .. {
+		InitCommand=function(self)
+			self:y((i*43)-150)
 		end
 	}
 end
